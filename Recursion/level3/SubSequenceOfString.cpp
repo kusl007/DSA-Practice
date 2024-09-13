98b5815f869f9c799f82a8842a15b1c4ff248426
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-09-13 13:14:55 +0545

    level 3 completed

--- Recursion/level3/SubSequenceOfString.cpp
+++ Recursion/level3/SubSequenceOfString.cpp
@@ -2,3 +2,41 @@
 //it requires inclusion and exclusion
 //vvi in recursion
 //the ans will be 2^n
+
+
+
+#include <iostream>
+using namespace std;
+
+void SubSequenceFinder(string str,string output ,int i){
+   if(i>=str.length()){
+ cout<<"the output are => "<<output<<endl;
+       return;
+   }
+   
+   //exclude
+    SubSequenceFinder(str,output,i+1);
+   
+   //include
+   output.push_back(str[i]);
+    SubSequenceFinder(str,output,i+1);
+
+    //if wanna write exclude in bottom must remove the included part in top
+      //exclude
+    //   output.pop_back();
+    // SubSequenceFinder(str,output,i+1);
+   
+   
+  
+}
+
+int main() {
+   string str="abc";
+   string output="";
+   int i=0;
+   SubSequenceFinder(str,output,i);
+    
+   
+
+    return 0;
+}
