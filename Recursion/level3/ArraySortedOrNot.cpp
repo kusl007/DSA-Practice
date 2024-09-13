334f9d830f9b2b50ca34c2c54313e137ef0752e1
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-09-13 13:37:28 +0545

    level 3 completed

--- Recursion/level3/ArraySortedOrNot.cpp
+++ Recursion/level3/ArraySortedOrNot.cpp
@@ -3,31 +3,35 @@
 #include <vector>
 using namespace std;
 
-bool checkSorted(vector<int> &arr, int& size,int i){
-    if(i==size-1){
+bool checkSorted(vector<int> &arr, int &size, int i)
+{
+    if (i == size - 1)
+    {
         return true;
     }
-    if(arr[i+1]<arr[i]){
+    if (arr[i + 1] < arr[i])
+    {
         return false;
     }
-    
-    return checkSorted(arr,size,i+1);
-  
+
+    return checkSorted(arr, size, i + 1);
 }
 
-int main() {
-   vector <int> v{1,2,3,4,5,6,1};
-    int size=v.size();
-    int i=0;
-   bool ans= checkSorted(v,size,i);
-   cout<<ans<<endl;
-   if(ans){
-       cout<<"The array is sorted";
-   }
-   else{
-       cout<<"The array is not sorted";
-   }
-   
+int main()
+{
+    vector<int> v{1, 2, 3, 4, 5, 6, 1};
+    int size = v.size();
+    int i = 0;
+    bool ans = checkSorted(v, size, i);
+    cout << ans << endl;
+    if (ans)
+    {
+        cout << "The array is sorted";
+    }
+    else
+    {
+        cout << "The array is not sorted";
+    }
 
     return 0;
 }
