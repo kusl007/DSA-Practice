f45dcfbd70b64f4bf86dd29d31674d019ec00369
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-09-14 12:53:09 +0545

    level 4 completed

--- Recursion/level4/MaxSumOfNonAdjacentElements.cpp
+++ Recursion/level4/MaxSumOfNonAdjacentElements.cpp
@@ -9,42 +9,35 @@
 // include i=i+2;
 // exclude i=i+1;
 
-
-
 #include <iostream>
 #include <limits.h>
-#include<vector>
+#include <vector>
 using namespace std;
 
-void solve(vector <int> &arr,int sum,int &maxi,int i){
-    //base case
-    if(i>=arr.size()){
-        maxi=max(sum,maxi);
+void solve(vector<int> &arr, int sum, int &maxi, int i)
+{
+    // base case
+    if (i >= arr.size())
+    {
+        maxi = max(sum, maxi);
         return;
-    
     }
-    //include
-    solve(arr,sum+arr[i],maxi,i+2);
-    //exclude
-    solve(arr,sum,maxi,i+1);
-    
-    
-
-
-
+    // include
+    solve(arr, sum + arr[i], maxi, i + 2);
+    // exclude
+    solve(arr, sum, maxi, i + 1);
 }
 
+int main()
+{
 
-int main() {
-    
-vector <int> arr{1,2,4,9};
-int sum =0;
-int maxi=INT_MIN;
-int i=0;
+    vector<int> arr{1, 2, 4, 9};
+    int sum = 0;
+    int maxi = INT_MIN;
+    int i = 0;
 
-solve(arr,sum,maxi,i);
-cout<<maxi<<endl;
-
+    solve(arr, sum, maxi, i);
+    cout << maxi << endl;
 
     return 0;
 }
