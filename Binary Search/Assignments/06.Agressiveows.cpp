029b1f81943d9d29d4f16569e7e9801874009438
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-10-24 12:11:15 +0545

    Binary Search assignments partially completed

--- Binary Search/Assignments/06.Agressiveows.cpp
+++ Binary Search/Assignments/06.Agressiveows.cpp
@@ -41,3 +41,42 @@
     }
 
 
+// ************** gfg soln *****************
+//https://www.geeksforgeeks.org/problems/aggressive-cows/0
+
+class Solution {
+public:
+bool isPossibleSolution( vector<int> &stalls,int k,int mid){
+    int cow=1;
+    int pos=stalls[0];
+    for(int i=1;i<stalls.size();i++){
+        if(stalls[i]-pos>=mid){
+            cow++;
+            pos=stalls[i];
+        }
+        if(cow==k){
+            return true;
+        }
+    }
+    return false;
+}
+
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        sort(stalls.begin(),stalls.end());
+        int start=0;
+        int end=stalls[stalls.size()-1]-stalls[0];
+        int ans=-1;
+        while(start<=end){
+            int mid=(start+end)>>1;
+            if(isPossibleSolution(stalls,k,mid)){
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+};
