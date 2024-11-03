e8e4cf01483e94cbcc52fb26fd5265e89f4c6467
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-11-03 11:51:33 +0545

    top 150 leetcode Math Problems started

---
+++ leetcode top 150/Math/02.PlusOne.cpp
@@ -0,0 +1,21 @@
+//66. Plus One
+//https://leetcode.com/problems/plus-one/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
---
+++ leetcode top 150/Math/03.FactorialTrailingZeroes.cpp
@@ -0,0 +1,2 @@
+//172. Factorial Trailing Zeroes
+//https://leetcode.com/problems/factorial-trailing-zeroes/description/?envType=study-plan-v2&envId=top-interview-150
